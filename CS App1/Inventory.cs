40693f5ddaeb604399b5de46d860207606ad6b99
Artit Kiuwilai <first087@gmail.com>
2016-02-12 17:25:33 +0700

    [CS] Finish class Inventory

--- CS App1/Inventory.cs
+++ CS App1/Inventory.cs
@@ -5,5 +5,44 @@ using System.Text;
 
 namespace CS_App1 {
     public class Inventory {
+        private List<Guitar> guitars;
+
+        public Inventory() {
+            this.guitars = new List<Guitar>();
+        }
+
+        public void addGuitar(string serialNumber, double price, string builder, string model, string type, string backWood, string topWood) {
+            Guitar guitar = new Guitar(serialNumber, price, builder, model, type, backWood, topWood);
+            this.guitars.Add(guitar);
+        }
+
+        public Guitar getGuitar(string serialNumber) {
+            for (int i = 0; i < this.guitars.Count; i++) {
+                Guitar guitar = this.guitars[i];
+
+                if (guitar.getSerialNumber().Equals(serialNumber)) {
+                    return guitar;
+                }
+            }
+
+            return null;
+        }
+
+        public Guitar search(Guitar searchGuitar) {
+            for (int i = 0; i < this.guitars.Count; i++) {
+                Guitar guitar = this.guitars[i];
+
+                string builder = searchGuitar.getBuilder();
+                if ((builder != null) && (!builder.Equals("") && (!builder.Equals(guitar.getBuilder())))) {
+                    continue;
+                }
+
+                // TODO : compare each property of the guitar
+
+                return guitar;
+            }
+
+            return null;
+        }
     }
 }
